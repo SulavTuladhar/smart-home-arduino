3d611d20b38c3298f68efaa37da55cc7754d3dff
Sulav Tuladhar <sulavtuladhar@gmail.com>
2026-07-11 12:51:17 +0545

    refactor(server): device registration refactor

--- server/src/modules/device/device.service.ts
+++ server/src/modules/device/device.service.ts
@@ -1,5 +1,6 @@
 import { Relay } from "../relays/relay.entity";
 import { relayRepository } from "../relays/relay.repository";
+import { relayService } from "../relays/relay.service";
 import { Device } from "./device.entity";
 import { deviceRepository } from "./device.repository";
 import { DeviceRegisteration } from "./device.types";
@@ -17,21 +18,9 @@ export class DeviceService {
         device.relayCount = registration.relayCount;
         device.online = true;
 
-        await deviceRepository.save(device);
+        device = await deviceRepository.save(device);
 
-        for(const relayRegistration of registration.relays){
-            let relay = await relayRepository.findByDeviceAndChannel(registration.deviceId, relayRegistration.channel);
-            if(!relay){
-                relay = new Relay();
-                relay.device = device;
-                relay.channel = relayRegistration.channel;
-                relay.name = `Relay-${relayRegistration.channel}`;
-                relay.desiredState = false;
-                relay.acutalState = false;
-            }
-            relay.gpio = relayRegistration.gpio;
-            await relayRepository.save(relay);
-        }
+        await relayService.syncRelays(device, registration);
 
         console.log(`Device ${registration.deviceId} registered with ${registration.relayCount} relays`);
     }
--- server/src/modules/relays/relay.service.ts
+++ server/src/modules/relays/relay.service.ts
@@ -1,9 +1,30 @@
+import { Device } from "../device/device.entity";
+import { DeviceRegisteration } from "../device/device.types";
 import { DeviceStateMessage } from "../mqtt/mqtt.types";
+import { Relay } from "./relay.entity";
 import { relayRepository } from "./relay.repository";
 
 export class RelayService {
     async updateRelayState(message: DeviceStateMessage): Promise<void> {
         await relayRepository.updateState(message.device_id, message.channel, message.state);
+    }
+
+    async syncRelays(device: Device, registration: DeviceRegisteration): Promise<void> {
+        for(const relayRegistration of registration.relays){
+            let relay = await relayRepository.findByDeviceAndChannel(registration.deviceId, relayRegistration.channel);
+            if(!relay){
+                relay = new Relay();
+                relay.device = device;
+                relay.channel = relayRegistration.channel;
+                relay.name = `Relay-${relayRegistration.channel}`;
+                relay.desiredState = false;
+                relay.acutalState = false;
+            }
+            relay.gpio = relayRegistration.gpio;
+            await relayRepository.save(relay);
+        }
+
+        console.log(`Synchronized ${registration.relays.length} relays for ${registration.deviceId}`);
     }
 }
 
